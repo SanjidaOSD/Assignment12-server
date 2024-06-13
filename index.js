c36bd8e7af87b303087b67fa69c452fa83e48871
unknown <sanjidaema81@gmail.com>
2024-06-14 01:28:56 +0600

    Delete pet and Adoption status update functionality added

--- index.js
+++ index.js
@@ -132,6 +132,14 @@ async function run() {
             res.send(result)
         })
 
+        // Delete a pet data from db
+        app.delete('/pet-data-delete/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = { _id : new ObjectId(id)};
+            const result = await petCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
